4c2d48ed483b332f07fbdd98d48a706d69787770
Lucas Rodriguez Sinni <sinniknot@gmail.com>
2024-07-08 21:07:28 -0300

    Código feito por mim finalizado
    Este código foi feito inteiramente por mim, por este motivo, os conceitos de POO acabaram não sendo totalmente aplicados.
    
    O ponto principal de meu erro neste código foi ter me esquecido da possibilidade de criar um método com a condição IF na classe Student, deixando assim o programa principal mais limpo e orientado a objetos.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,39 @@
 package application;
 
+import java.util.Scanner;
+
+import entity.Student;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		Student std = new Student();
+		
+		std.name = sc.next();
+		std.grade1 = sc.nextDouble();
+		std.grade2 = sc.nextDouble();
+		std.grade3 = sc.nextDouble();
+	
+		
+		if(std.finalGrade() >= 60) {
+			
+			System.out.println();
+			System.out.println("FINAL GRADE = " + std.finalGrade());
+			System.out.println("PASS");
+			
+		}else {
+			
+			System.out.println();
+			System.out.println("FINAL GRADE = " + std.finalGrade());
+			System.out.println("FAILED");
+			System.out.println("MISSING " + (100 - std.finalGrade()) + " POINTS");
+			
+		}
+		
+		
+		sc.close();
 	}
 
 }
--- src/entity/Student.java
+++ src/entity/Student.java
@@ -1,5 +1,14 @@
 package entity;
 
 public class Student {
-
+	
+	public String name;
+	public double grade1;
+	public double grade2;
+	public double grade3;
+	
+	public double finalGrade(){
+		return grade1 + grade2 + grade3;
+	}
+	
 }
