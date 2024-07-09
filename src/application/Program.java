9a77f8f7047cef8d4758a4ec1a90546b7a31a7e2
Lucas Rodriguez Sinni <sinniknot@gmail.com>
2024-07-08 21:12:13 -0300

    Código feito pelo Prof. Nélio ajustado.
    Realizando o commit do código correto feito pelo Prof. Nélio.
    
    O único ponto de diferença entre meu código e o do Prof. Nélio é apenas o método missingPoints(), onde em meu código eu acabei fazendo direto no programa.
    
    Outro ponto menor é a formatação da nota, onde o Prof. Nélio inseriu ou SysOut a formatação %.2f, no qual acabei ignorando no meu código.

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,32 +7,26 @@ import entity.Student;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		Student std = new Student();
-		
+
 		std.name = sc.next();
 		std.grade1 = sc.nextDouble();
 		std.grade2 = sc.nextDouble();
 		std.grade3 = sc.nextDouble();
-	
-		
-		if(std.finalGrade() >= 60) {
-			
-			System.out.println();
-			System.out.println("FINAL GRADE = " + std.finalGrade());
-			System.out.println("PASS");
-			
-		}else {
-			
-			System.out.println();
-			System.out.println("FINAL GRADE = " + std.finalGrade());
+
+		System.out.printf("FINAL GRADE: %.2f%n", std.finalGrade());
+
+		if (std.finalGrade() < 60.0) {
 			System.out.println("FAILED");
-			System.out.println("MISSING " + (100 - std.finalGrade()) + " POINTS");
-			
+			System.out.printf("MISSING %.2f POINTS%n", std.missingPoints());
+
+		} else {
+			System.out.println("PASS");
+
 		}
-		
-		
+
 		sc.close();
 	}
 
